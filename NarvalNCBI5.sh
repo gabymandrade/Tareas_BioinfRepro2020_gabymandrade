e705c960ac42ec1868e8b6dc228b6b20b468ada9
gabymandrade <59860116+gabymandrade@users.noreply.github.com>
2020-01-20 21:26:42 -0600

    Tarea 1.3

---
+++ NarvalNCBI5.sh
@@ -0,0 +1,16 @@
+## Este script baja 5 secuencias de Monodon monoceros de NCBI
+# Gabriela Martínez Andrade, 2020
+
+# Crear directorio para guardar datos
+mkdir Narval5
+
+# Bajar datos de NCBI
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=1655748997" > Narval5/Narvales1.fasta
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=1655748996" > Narval5/Narvales2.fasta
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=1655748995" > Narval5/Narvales3.fasta
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=1655748994" > Narval5/Narvales4.fasta
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=1655748993" > Narval5/Narvales5.fasta
+
+# Revisar qué secuencias se bajaron
+grep ">" Narval5/Narvales.*fasta
+
